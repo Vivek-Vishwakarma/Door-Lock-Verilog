8e7deee3405c12b79244df7d8636ddba519b94f2
Vivek-Vishwakarma <vivek.vishwakarma10022002@gmail.com>
2022-03-24 22:00:55 +0530

    some tb remaining

---
+++ door_lock/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/ise/Door Lock/doorlock/t_door_lock_top_isim_beh.exe" -prj "/home/ise/Door Lock/doorlock/t_door_lock_top_beh.prj" "work.t_door_lock_top" "work.glbl" 
--- doorlock/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/ise/Door Lock/doorlock/t_password_getter_isim_beh.exe" -prj "/home/ise/Door Lock/doorlock/t_password_getter_beh.prj" "work.t_password_getter" "work.glbl" 
